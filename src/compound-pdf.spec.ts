8889c71bb44ffafe53abafacee47faded34454b4
Nikita Moshensky <nikita@moshensky.com>
2018-01-14 01:58:11 +0200

    work around travis-ci docker-chrome issue

--- src/compound-pdf.spec.ts
+++ src/compound-pdf.spec.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs'
 import { join } from 'path'
 
-import { mkCompoundPdf } from '../src/compound-pdf'
+import { mkCompoundPdf, MkPdfOptions } from '../src/compound-pdf'
 import { PrintMargin, pageSizeInMM } from '../src/types'
 import { Millimeters } from './index'
 import { compareToExpectedMultiple, compareToExpected } from './utils/test-helpers'
@@ -21,56 +21,70 @@ const margin: PrintMargin = {
   left: Millimeters.of(17),
 }
 
+// TODO: find out how to run without those options
+// Travis-CI Docker image workaround
+const puppeteerOptions: MkPdfOptions = {
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
+}
+
 describe('mkCompoundPdf()', () => {
   it('text footer', async () => {
-    const pdfBuffer = await mkCompoundPdf([
-      {
-        pdfContent: firstPageHtml,
-        footer: {
-          type: 'TextHeaderFooter',
-          left: {
-            fontPath: fontPath,
-            text: 'https://github.com/moshensky/html-to-pdf-converter',
-            size: footerFontSize,
-            underline: true,
-            color: 0x0060bf, // '#0060bf'
+    const pdfBuffer = await mkCompoundPdf(
+      [
+        {
+          pdfContent: firstPageHtml,
+          footer: {
+            type: 'TextHeaderFooter',
+            left: {
+              fontPath: fontPath,
+              text: 'https://github.com/moshensky/html-to-pdf-converter',
+              size: footerFontSize,
+              underline: true,
+              color: 0x0060bf, // '#0060bf'
+            },
+            right: {
+              fontPath: fontPath,
+              text: 'Page {page} of {pages}',
+              size: footerFontSize,
+              color: 0x000000, // '#000000'
+            },
           },
-          right: {
-            fontPath: fontPath,
-            text: 'Page {page} of {pages}',
-            size: footerFontSize,
-            color: 0x000000, // '#000000'
-          },
+          margin,
+          pageSize: pageSizeInMM.A4.portrait,
         },
-        margin,
-        pageSize: pageSizeInMM.A4.portrait,
-      },
-    ])
+      ],
+      puppeteerOptions,
+    )
 
     await compareToExpected('textFooter', pdfBuffer, false)
   })
 
   it('html footer', async () => {
-    const pdfBuffer = await mkCompoundPdf([
-      {
-        pdfContent: firstPageHtml,
-        footer: {
-          type: 'HtmlHeaderFooter',
-          html: footerHtml,
+    const pdfBuffer = await mkCompoundPdf(
+      [
+        {
+          pdfContent: firstPageHtml,
+          footer: {
+            type: 'HtmlHeaderFooter',
+            html: footerHtml,
+          },
+          margin,
+          pageSize: pageSizeInMM.A4.portrait,
         },
-        margin,
-        pageSize: pageSizeInMM.A4.portrait,
-      },
-      {
-        pdfContent: resultsHtml,
-        footer: {
-          type: 'HtmlHeaderFooter',
-          html: footerHtml,
+        {
+          pdfContent: resultsHtml,
+          footer: {
+            type: 'HtmlHeaderFooter',
+            html: footerHtml,
+          },
+          margin,
+          pageSize: pageSizeInMM.A4.landscape,
         },
-        margin,
-        pageSize: pageSizeInMM.A4.landscape,
-      },
-    ])
+      ],
+      puppeteerOptions,
+    )
 
     await compareToExpectedMultiple('htmlFooter', pdfBuffer, false)
   })
